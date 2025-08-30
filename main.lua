9eb381dbc1700a670c83d2dc0bfb5feb18212322
Hawker <kovujacksonbentley@gmail.com>
2025-08-30 16:19:58 -0400

    Get logging working

--- main.lua
+++ main.lua
@@ -1,11 +1,18 @@
 --- STEAMODDED HEADER
 --- MOD_NAME: The Joker Finder (WIP)
---- MOD_ID: JokerFinder (WIP)
+--- MOD_ID: JokerFinder
 --- MOD_AUTHOR: [Hawker]
 --- MOD_DESCRIPTION: This mod tells the user if the Throwback joker is in small or big blind
 
 ----------------------------------------------
 ------------MOD CODE -------------------------
+
+G.E_MANAGER:add_event(Event({
+    func = function() 
+        sendDebugMessage("Joker mod is running")
+        return true 
+    end
+}))
 
 ----------------------------------------------
 ------------MOD CODE END----------------------
