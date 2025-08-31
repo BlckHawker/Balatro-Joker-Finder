82245ac75b4ddf40f6bbe4dc38bc8ffbb266f938
Hawker <kovujacksonbentley@gmail.com>
2025-08-31 17:22:00 -0400

    Show seed in UI

--- src/interface.lua
+++ src/interface.lua
@@ -1,5 +1,5 @@
 --- The Joker Finder - interface.lua
---
+
 -- The user interface components that display if the joker can be found with the parameters
 
 -- Append node for preview text to the HUD:
@@ -18,6 +18,11 @@ end
 -- Checks to see if the parametrized item is found within the filters
 function get_item_availability()
     return {n=G.UIT.C, config={align = "cm"}, nodes={
-            {n=G.UIT.T, config={text = "Hello world", colour = G.C.UI.TEXT_LIGHT, scale = 0.5}}
-         }}
+               {n=G.UIT.R, config={align = "cm"}, nodes={
+                  {n=G.UIT.T, config={text = tostring(G.GAME.pseudorandom.seed), colour = G.C.UI.TEXT_LIGHT, scale = 0.5}},
+               }},
+               {n=G.UIT.R, config={align = "cm"}, nodes={
+                  {n=G.UIT.T, config={text = tostring("Hello world"), colour = G.C.UI.TEXT_LIGHT, scale = 0.5}},
+               }}
+            }}
 end
---
+++ src/perform_analysis.lua
@@ -0,0 +1,7 @@
+--- The Joker Finder - perform_analysis.lua
+
+-- Calculates what jokers the user is going to see within a certain ante
+
+
+function get_joker(ante)
+end
